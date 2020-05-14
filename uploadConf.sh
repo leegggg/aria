4fbecbca3097638f3773ce18efd632af0dc90f38
Debian <admin@aria2>
2020-05-14 13:45:51 +0000

    config upload

--- uploadConf.sh
+++ uploadConf.sh
@@ -2,7 +2,7 @@ set -x
 git add .
 git commit -a -m 'config upload'
 git push
-ssh admin@aria2.lan.linyz.net wget https://raw.githubusercontent.com/leegggg/aria/master/aria.conf -O /etc/aria2.conf
-ssh admin@aria2.lan.linyz.net sudo systemctl restart aria2c
+wget https://raw.githubusercontent.com/leegggg/aria/master/aria.conf -O /etc/aria2.conf
+sudo systemctl restart aria2c
 sleep 2
-ssh admin@aria2.lan.linyz.net sudo systemctl status aria2c
+sudo systemctl status aria2c
